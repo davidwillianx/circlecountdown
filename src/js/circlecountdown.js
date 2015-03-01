293223b0e46d40cd080d8ffde3cf198c443a5687
davidwillianx <davidcbsi@gmail.com>
2015-03-01 17:44:46 -0300

    easypie added

---
+++ src/js/circlecountdown.js
@@ -0,0 +1,18 @@
+var options = {
+  scaleColor: false,
+  trackColor: 'rgba(255,255,255,0.3)',
+  barColor: '#E7F7F5',
+  lineWidth: 6,
+  lineCap: 'butt',
+  size: 95
+};
+
+(function(){
+
+  $('.pie').easyPieChart(options);
+
+  // $('.pie').each(function(index,pie){
+  //     chars.push(new EasyPieChart(pie,options));
+  // });
+
+})();
