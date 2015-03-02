c83e77c803d0892b55b89439b270aa4f5e669fc6
davidwillianx <davidcbsi@gmail.com>
2015-03-02 17:05:54 -0300

    draw inside plugin

--- src/js/circlecountdown.js
+++ src/js/circlecountdown.js
@@ -8,7 +8,41 @@
 // };
 
 ;(function($){
-  $.fn.circleCountdown = function(options){
+
+  var defaults = {
+    scaleColor: false,
+    trackColor: 'rgba(255,255,255,0.3}',
+    barColor: '#E7F7F5',
+    lineWidth: 6,
+    lineCap: 'butt',
+    size: 95,
+    time: 20
+  };
+
+  var drawCountdown =  function(element,options){
+    // Magic it will happend
+    var settings = $.extend({} ,defaults,options);
 
   }
+
+  drawCountdown.prototype = {
+    init: function(){
+
+    }
+
+  }
+
+  $.fn.circleCountdown = function(options){
+
+      var countdown = function(){
+        var element = $(this);
+         drawCountdown(element,options);
+
+      };
+
+      return this.each(countdown);
+  };
+
+
+
 })(jQuery);
